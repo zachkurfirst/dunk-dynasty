18e818ee26c9de7f180067bb5c3bde64e8d5aeb9
Zach Kurfirst <zachmk11@gmail.com>
2024-01-14 13:54:44 -0500

    set up home and about request/response cycles, add base.html, template inheritance and static css file

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# HOME VIEW
+def home(request):
+    return render(request, 'home.html')
+
+# ABOUT VIEW
+def about(request):
+    return render(request, 'about.html')
