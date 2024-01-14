bf9a42bc7d68a61fc34ce22fdf4caa681559592e
Zach Kurfirst <zachmk11@gmail.com>
2024-01-14 15:52:54 -0500

    render franchises with dummy data to index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+franchises = [
+    {'city': 'Cincinnati',
+     'name': 'Pythons',
+     'motto': 'Winning is the only option.',
+     'established': 'Jan 2024'},
+     {'city': 'Buffalo',
+     'name': 'Wings',
+     'motto': 'Buffalo built.',
+     'established': 'Jan 2024'},
+     {'city': 'Seattle',
+      'name': 'Swish',
+      'motto': 'Bringing hoops back to where it belongs.',
+      'established': 'Jan 2024'},
+]
+
 # Create your views here.
 
 # HOME VIEW
@@ -9,3 +24,9 @@ def home(request):
 # ABOUT VIEW
 def about(request):
     return render(request, 'about.html')
+
+# FRANCHISES INDEX
+def franchises_index(request):
+    return render(request, 'franchises/index.html', {
+        'franchises': franchises
+    })
