0efb03a0120c338c09e689d65ed97ac162d2ebed
Zach Kurfirst <zachmk11@gmail.com>
2024-01-16 12:43:11 -0500

    create 1:M relationship - user to franchises

--- dunkdynasty/settings.py
+++ dunkdynasty/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions', # track when user logged in/out
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+# Import the User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Franchise(models.Model):
@@ -7,6 +9,8 @@ class Franchise(models.Model):
     name = models.CharField(max_length=25)
     motto = models.CharField(max_length=100)
     established = models.DateField(auto_now_add=True)
+    # adds user_id to FK column
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.city} {self.name}"
