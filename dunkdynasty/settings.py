857ec04555f7ebab5ada9b1ae6e3bf4dcc35057a
Zach Kurfirst <zachmk11@gmail.com>
2024-01-16 14:50:18 -0500

    dynamic nav bar, implement login and logout functionality

--- dunkdynasty/settings.py
+++ dunkdynasty/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/franchises/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
