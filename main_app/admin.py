38549ce7d0d0c39f13fd62669bb40927ebd219d4
Zach Kurfirst <zachmk11@gmail.com>
2024-01-17 23:35:31 -0500

    complete s3 image upload functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 # Import model
-from .models import Franchise
+from .models import Franchise, Photo
 # Register your models here.
 admin.site.register(Franchise)
+admin.site.register(Photo)
